df71a53ee6d30505e85debe583264c0d22ba428d
Akira Funahashi <funa@bio.keio.ac.jp>
2024-09-23 15:27:04 +0900

    Modify the files for likelihood

--- main.c
+++ main.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
+#include "experiment.h"
 #include "sbml/SBMLTypes.h"
 
-int main (void)
-{
-  SBMLDocument_t *d;
-  int level, version;
+double likelihood_simple(double init_S1, double init_S2, double k) {
+  /* 変数定義 */
+  double likelihood = 0.0;
 
-  d = readSBML("mapk.xml");
-  level = SBMLDocument_getLevel(d);
-  version = SBMLDocument_getVersion(d);
 
-  printf("Level %d, Version %d\n", level, version);
+
+  /* t = 0 ~ 10.0 までシミュレーションを実行 */
+
+
+
+
+  /* 与えられたkの値に応じてlikelihoodを計算 */
+
+
+
+
+  return likelihood;
+}
+
+int main(void) {
+  double k;
+  double likelihood;
+  
+  printf("k , likelihood\n");
+  /* simple.xmlを読み込み尤度計算に必要な情報を取得する */
+  SBMLDocument_t *d = readSBML("simple.xml");
+
+
+
+  /* 様々なパラメータで尤度を計算する */
+  for(k = 0.0 ; k < 1.01 ; k = k + 0.01) {
+    
+  }
+
   SBMLDocument_free(d);
   return 0;
 }
